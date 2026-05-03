cd8bcaa942fcc6bfb5dc2e8daf0a6f3cfd74caba
joel <angeljoeltoledo@gmail.com>
2026-05-03 18:57:17 -0300

    Resolucion Problema N3

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -13,6 +13,10 @@ namespace Dsw2026Ej8
             Problema2 resumen = new Problema2();
             string resumenVenta = resumen.CrearResumenVenta(productCode: 1100, productDescription: "Celular", quantity: 2, unitPrice: 1299.99m);
             Console.WriteLine(resumenVenta);    
+
+            Problema3 compara = new Problema3();
+            string comparacion = compara.CompararCopias(originalValue: 1, product: new Product { descripcion = "Producto original" });
+            Console.WriteLine(comparacion);
         }
     }
 }
