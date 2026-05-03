7e8a0fbd98b653b4b73082a11346a4773e3c4e1e
joel <angeljoeltoledo@gmail.com>
2026-05-03 18:08:53 -0300

    Resolucion Problema N1

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Dsw2026Ej8
+﻿using static Dsw2026Ej8.ProductHelper;
+
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProductHelper helper = new ProductHelper();
+
+            string etiqueta = helper.ObtenerEtiquetaProducto(code: 1001,  description: "Laptop", price: 1599.99m);
+
+            Console.WriteLine(etiqueta);
         }
     }
 }
+
